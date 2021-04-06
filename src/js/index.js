d47a679a1b92edd6b008e6bcef1b0e82026ad8d5
Tomasz Bauman <tomaszbauman03@gmail.com>
2021-04-06 21:09:36 +0200

    save button functional

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,26 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const historia =localStorage.getItem('historia');
+let result = '';
+
+if (historia) {
+    result = historia;
+}
+
+const text = document.querySelector('.text--js');
+text.value = result;
+
+const save = document.querySelector('.save--js');
+
+save.addEventListener('click', () => {
+    localStorage.setItem('historia',text.value);
+});
+
+
+// localStorage.setItem('historia','');
+
+// const load = document.querySelector('.load--js');
+
+// localStorage.getItem('historia');
